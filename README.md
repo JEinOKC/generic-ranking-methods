590569bf8ffd479fa014975ca6fcd23745cd9825
James England <jamgland@gmail.com>
2014-12-27 11:00:41 -0600

    first push to github

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-generic-ranking-methods
-=======================
 
-Basic Methods To Turn Games/Results into a set of rankings
+= generic-ranking-methods
+
+node.js implementation to generate sports rankings
+
+This project is maintained by {James R England}[http://www.jamesrengland.com]
+
+= Documentation
+
+To Be Continued...
+
+= Installation
+
+    npm install generic-ranking-methods
+
+= Usage
+
+To Be Continued...
+
+= License
+
+This project is released under The MIT License
+
+Copyright (c) 2014, James R England
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,153 @@
+var sylvester 	= require('sylvester'),
+  Matrix = sylvester.Matrix,
+  Vector = sylvester.Vector;
+
+// constructor
+function RankingMethods () {};
+
+
+RankingMethods.prototype.Run = function(teamArray, schedule){
+	//initialize the matrices based on the sizes of our queries
+	var myMatrices = rankingMethods.matrixTemplates(schedule.length, teamArray.length);
+
+	//populate the matrices
+	myMatrices = rankingMethods.populateMatrices(myMatrices,teamArray,schedule);
+
+	var results = null;
+	try{
+		results = rankingMethods.matrixMagic(myMatrices).elements;					
+	}
+	catch(err){
+		throw "could not run the ranking process";
+	}
+
+	return results;
+};
+
+	
+
+RankingMethods.prototype.matrixMagic = function(myMatrices){
+	
+	var matrix_X = myMatrices.gameMatrix;//an nxn matrix of games played
+	var matrix_Y = myMatrices.diffMatrix;//an nx1 matrix of game results
+	var matrix_xTrans = matrix_X.transpose();
+	var xTx = matrix_xTrans.x(matrix_X);
+	var xTy = matrix_xTrans.x(matrix_Y);
+
+
+	var xTx_array = xTx.elements;
+	var xTy_array = xTy.elements;
+
+	for(i=0;i<xTx_array[xTx_array.length-1].length;i++)
+		xTx_array[xTx_array.length-1][i] = 1;
+
+	xTx = Matrix.create(xTx_array);
+
+	for(i=0;i<xTy_array[xTy_array.length-1].length;i++)
+		xTy_array[xTy_array.length-1][i] = 0;
+
+	xTy = Matrix.create(xTy_array);
+	
+	var xTx_Inverse = xTx.inv();
+	if(xTx_Inverse !== null){
+		console.log('inverse was created. Things going well..');
+		return xTx_Inverse.x(xTy);	
+	}
+	
+
+	console.log("matrix is singular. Cannot create a ranking");
+
+	throw "matrix is singular. Cannot create a ranking";
+	// return null;
+};
+
+
+RankingMethods.prototype.matrixTemplates = function(gameCount, teamCount){
+	
+	/*The Java we are emulating:
+		double[][] gameMatrix = new double[GameArray.size()][TeamArray.size()];
+        double[][] diffMatrix = new double[GameArray.size()][1];
+	*/
+
+	myMatrices = {
+		'gameMatrixArray' : rankingMethods.create2Darray(gameCount, teamCount),
+		'diffMatrixArray' : rankingMethods.create2Darray(gameCount, 1)
+	};
+
+	return myMatrices;
+
+};
+
+RankingMethods.prototype.populateMatrices = function(myMatrices,TeamArray,scheduleArray){
+	
+
+	/*
+
+		the schedule array must contain an array of games
+
+		The Game object must contain:
+		{
+			'home_team_code': ...,
+			'opponent_code': ...
+			'ptDiff' : 0, //difference between winner or loser. could be exact score, adjusted score, percentage, etc
+			'homeRep' : -1|1,//-1 indicates a loss
+			'awayRep' : 1|-1//1 indicates a win
+		}
+
+		a minimal game object can be found using the minimalGameObject method
+	*/
+
+	for(i=0;i<scheduleArray.length;i++){
+		var Game = scheduleArray[i].calcs;
+
+
+		for(j=0;j<TeamArray.length;j++){
+			var tmpTeam = TeamArray[j];
+			if(tmpTeam['team_id'] == Game.home_team_code){
+				// console.log("MATCH!!");
+				myMatrices.gameMatrixArray[i][j] = Game.homeRep;
+			}
+			else if (tmpTeam['team_id'] == Game.opponent_code){
+				// console.log("MATCH!!");
+				myMatrices.gameMatrixArray[i][j] = Game.awayRep;
+			}	
+			else
+				myMatrices.gameMatrixArray[i][j] = 0;
+		}
+
+		myMatrices.diffMatrixArray[i][0] = Game.ptDiff;
+
+	}
+
+	myMatrices.gameMatrix = Matrix.create(myMatrices.gameMatrixArray);
+	myMatrices.diffMatrix = Matrix.create(myMatrices.diffMatrixArray);
+
+	return myMatrices;
+};
+
+RankingMethods.prototype.minimalGameObject = function(){
+	return {
+		'home_team_code': 'TEAM1',
+		'opponent_code': 'TEAM2',
+		'ptDiff' : 0, //difference between winner or loser. could be exact score, adjusted score, percentage, etc
+		'homeRep' : -1,//-1 indicates a loss
+		'awayRep' : 1//1 indicates a win
+	};
+}
+
+RankingMethods.prototype.create2Darray = function(x,y) {
+    var arr = new Array(x);
+
+    for(i=0;i<x;i++){
+    	arr[i] = new Array(y);
+    	for(j=0;j<y;j++){
+    		arr[i][j]=0;
+    	}
+    }
+
+    
+    return arr;
+}
+
+
+var rankingMethods = module.exports = exports = new RankingMethods;
